75304ef63bb5cb78cf25a7e6cbab9d381878ba95
Danilo Arthur Bertelli-BERTELLI <bertelli@motorola.com>
2025-03-11 13:31:06 -0300

    Add console logging for testing environment variable in App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import "./App.css";
 import AppRoutes from "./routes";
 
 function App() {
+  console.log("Testing...")
+  const name = process.env.MY_NAME as string
+  console.log(`Testing name = ${name}`)
   return <AppRoutes />;
 }
 
